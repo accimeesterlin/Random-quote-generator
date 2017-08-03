6af6f32443e7f51b80d723bff42c7e2bc57a21d1
Esterling Accime <esterling@brainbytescreative.com>
2017-08-02 22:57:15 -0400

    Adding some comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,52 @@
 
-// Caching
+// Caching the DOM
 var button = document.getElementById("button");
 
 var data = ['paper','scissor', 'rock']; // array
 
-
+// Global Variables
 var chances = 4;
-
 var win = 0;
 
+// Declaring a function
+// Storing a function in memory
 function generate(first) {
 	
 	var random = Math.floor(Math.random() * first.length);
 
 	var val = first[random];
 
-	//console.log(val);
-
-	return val;
+	return val; // making the function equal to the actual value
 }
 
 
-
+// Declaring a function named selected
 function select() {
 
-	var result = generate(data);
+	var result = generate(data); // invoking the generate function
 
 	console.log(result);
   
+  	// Conditional logic
 	if(result === 'paper'){
-		chances--;
+		chances--; // decrement things in JavaScript
 
 		if(chances <= 0){
 
 			alert("Sorry man, you lost!!!");
 
-			chances = 4;
+			chances = 4; // hoisting
 		}
 
 		console.log("Chances: " + chances);
 	} else{
-		win++;
+		win++; // increment
 
 		if(win >= 5){
 			
 			alert("Yupeeee!, you made!!");
 
-			win = 0;
+			win = 0; // hoisting
 		}
 
 		console.log("Win: " + win);
@@ -55,7 +55,7 @@ function select() {
 
 
 
-
+// Firing a click button
 button.addEventListener('click', select);
 
 
