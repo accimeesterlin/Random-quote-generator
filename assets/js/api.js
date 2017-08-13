91ba04dbb97229304e9281b3df0687968c3d11be
Esterling Accime <esterling@brainbytescreative.com>
2017-08-13 14:00:53 -0400

    Random Quote Generator

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,19 +1,18 @@
 
-// var backgroundColor = [
-// 	'red',
-// 	'blue',
-// 	'orange',
-// 	'green',
-// 	'purple'
-// ]
-// $.ajax();
-
-// Promises
+var backgroundColor = [
+	'red',
+	'blue',
+	'orange',
+	'green',
+	'purple'
+];
 
 var endpoint = 'https://twitterpopularapi.herokuapp.com/api?';
 
 var params = 'q=donaldtrump&count=10';
 
+
+// Making an API call
 $.ajax({
 	url:endpoint + params,
 	method:'GET',
@@ -23,16 +22,14 @@ $.ajax({
 
 	console.log(data);
 
-	var quotes = [];
+	var quotes = []; // empty array that's going to hold the data
 
+	// Looping through the data
 	for(var i = 0; i < data.statuses.length; i++){
 
-		quotes.push(data.statuses[i].text);
+		quotes.push(data.statuses[i].text); // pushing the empty array
 	
 	}
-
-	console.log(quotes);
-
 
 
 	var num = Math.floor(Math.random() * quotes.length); // random number
@@ -40,17 +37,15 @@ $.ajax({
 	var randomQuotes = quotes[num]; // random quotes
 
 
-	// var color = backgroundColor[num];
 
-
-	var content = $("<p>");
+	var content = $("<p>"); // creating a p element
 
 		content.html(randomQuotes);
 
 		content.attr({
 			"class":"quotes",
 			"data-name":"esterlin"
-		});
+		}); // adding different attributes
 
 		content.css({
 			"border":"3px solid rgba(0, 0, 0, 0.3)",
@@ -59,37 +54,38 @@ $.ajax({
 			"align-items": "center",
 			"height": "200px",
 			"box-shadow": "5px 5px 5px rgba(0, 0, 0, 0.5)"
-		});
+		}); // styling with css
 
-	$("#result").append(content);
+	$("#result").append(content); // adding p element into the DOM
 
 
 
-	// Set a click button
-
-	console.log(window);
-
+	// Click event on a button with id of random
 	$("#random").on('click', function () {
 
 		num = Math.floor(Math.random() * quotes.length); // random number
 
+		colorNum = Math.floor(Math.random() * backgroundColor.length); // random color num
+
 		randomQuotes = quotes[num]; // random quotes
 
-		// var color = backgroundColor[num];
+		var color = backgroundColor[colorNum];
 
 
-		content.empty();
+		content.empty(); // empty everything inside that element
 
-		content.append(randomQuotes);
+		content.append(randomQuotes); // adding content that element
 
-		// var color = backgroundColor[num];
+		var color = backgroundColor[num]; // random color
 
-		// $("body").css('background', color);
+		content.css('background', color); // adding random color
 
 	});
 
 
 })
+
+// In case we don't get any data from the API
 .fail(function (err) {
 
 	console.log(err);
